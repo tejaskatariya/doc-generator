4bdf37bd1003bbaae6bafd3a6c40a1786cac4e05
dheerajb <dheerajb@sahaj.ai>
2025-04-19 08:09:00 +0530

    Add testing support with jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+};
